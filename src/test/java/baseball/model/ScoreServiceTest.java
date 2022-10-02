cd8371c2f75b5936f57bdd25852483002a6a4cb9
sssukho <sssukho@gmail.com>
2022-10-02 21:05:32 +0900

    fix (기능#4 테스트 수정): 기능#4 테스트 코드 수정

--- src/test/java/baseball/model/ScoreServiceTest.java
+++ src/test/java/baseball/model/ScoreServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("기능#4 컴퓨터 측이 생성한 숫자와 사용자 입력 값 비교")
 class ScoreServiceTest {
     static ScoreService scoreService;
 
@@ -16,44 +17,67 @@ class ScoreServiceTest {
     }
 
     @Test
-    @DisplayName("기능#4 컴퓨터 측이 생성한 숫자와 사용자 입력 값 비교")
-    void test() {
-        // com: 425
-        Score case1 = getResultScore(generateTestCase("425", "123")); // 1strike
-        Score case2 = getResultScore(generateTestCase("425", "456")); // 1ball 1strike
-        Score case3 = getResultScore(generateTestCase("425", "789")); // nothing
-        // com: 713
-        Score case4 = getResultScore(generateTestCase("713", "123")); // 1ball 1strike
-        Score case5 = getResultScore(generateTestCase("713", "145")); // 1ball
-        Score case6 = getResultScore(generateTestCase("713", "671")); // 2ball
-        Score case7 = getResultScore(generateTestCase("713", "216")); // 1strike
-        Score case8 = getResultScore(generateTestCase("713", "713")); // 3strike
-
-        testResult(case1, 0, 1); // 1s
-        testResult(case2, 1, 1); // 1b 1s
-        testResult(case3, 0, 0); // nothing
-        testResult(case4, 1, 1); // 1b 1s
-        testResult(case5, 1, 0); // 1b
-        testResult(case6, 2, 0); // 2b
-        testResult(case7, 0, 1); // 1s
-        testResult(case8, 0, 3); // 3s
+    @DisplayName("1strike")
+    void test1() {
+        testResult(new TestCase("425", "123", new Score(1, 0)));
     }
 
-    void testResult(Score scoreResult, int ball, int strike) {
-        assertThat(scoreResult.getStrike()).isEqualTo(strike);
-        assertThat(scoreResult.getBall()).isEqualTo(ball);
+    @Test
+    @DisplayName("1strike 1ball")
+    void test2() {
+        testResult(new TestCase("425", "456", new Score(1, 1)));
     }
 
-    Score getResultScore(Round round) {
-        return scoreService.processResultScore(round);
+    @Test
+    @DisplayName("nothing")
+    void test3() {
+        testResult(new TestCase("425", "789", new Score(0, 0)));
     }
 
-    Round generateTestCase(String computerRandomNum, String userInput) {
-        Computer computer = new Computer(ComputerTest.newMap(computerRandomNum));
-        User user = new User(userInput);
-        Round round = new Round(Status.PLAYING);
-        round.setComputer(computer);
-        round.setUser(user);
-        return round;
+    @Test
+    @DisplayName("1ball")
+    void test4() {
+        testResult(new TestCase("713", "145", new Score(0, 1)));
+    }
+
+    @Test
+    @DisplayName("2ball")
+    void test5() {
+        testResult(new TestCase("713", "671", new Score(0, 2)));
+    }
+
+    @Test
+    @DisplayName("3strike")
+    void test6() {
+        testResult(new TestCase("713", "713", new Score(3, 0)));
+    }
+
+    static void testResult(TestCase testCase) {
+        assertThat(testCase.answerScore.getBall()).isEqualTo(testCase.resultScore.getBall());
+        assertThat(testCase.answerScore.getStrike()).isEqualTo(testCase.resultScore.getStrike());
+    }
+
+    static class TestCase {
+        public String computerRandomNum;
+        public String userInput;
+        public Score resultScore;
+        public Score answerScore;
+
+        public TestCase(String computerRandomNum, String userInput, Score answerScore) {
+            this.computerRandomNum = computerRandomNum;
+            this.userInput = userInput;
+            Round testRound = generateTestRound(computerRandomNum, userInput);
+            this.resultScore = scoreService.processResultScore(testRound);
+            this.answerScore = answerScore;
+        }
+
+        Round generateTestRound(String computerRandomNum, String userInput) {
+            Computer computer = new Computer(ComputerTest.newMap(computerRandomNum));
+            User user = new User(userInput);
+            Round round = new Round(Status.PLAYING);
+            round.setComputer(computer);
+            round.setUser(user);
+            return round;
+        }
     }
 }
